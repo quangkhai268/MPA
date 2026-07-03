6b25a4b7a1d3fa4de62dcbfdc751b551990d8c7e
quangkhai268 <quangkhai268@gmail.com>
2026-07-03 09:40:15 +0700

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mpa-snvr.onrender.com',
+  apiUrl: 'https://mpa-snvr.onrender.com/api',
   appName: 'BIDV MPA Dashboard',
   version: '1.0.0'
 };
