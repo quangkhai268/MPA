ba7de2d421e60ac00d9b2ad41fcb79fd27aedf5a
unknown <lekhai268@gmail.com>
2026-08-06 13:12:34 +0700

    change apiuri

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mpa-snvr.onrender.com/api',
+  //apiUrl: 'https://mpa-snvr.onrender.com/api',
+  apiUri: '/api'
   appName: 'BIDV MPA Dashboard',
   version: '1.0.0'
 };
